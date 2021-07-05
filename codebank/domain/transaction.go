6ad4da88f8dbb11cb2c3ab2003a04abce9c68843
Marco Aurélio Messa <marco.messa@ltmfidelidade.com.br>
2021-07-05 16:04:45 -0300

    feat: add transaction main method and repository

--- codebank/domain/transaction.go
+++ codebank/domain/transaction.go
@@ -6,6 +6,11 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
+type TransactionRepository interface {
+	SaveTransaction(transaction Transaction, creditCard CreditCard) error
+	GetCreditCard(creditCard CreditCard) (CreditCard, error)
+	CreateCreditCard(creditCard CreditCard) error
+}
 type Transaction struct {
 	ID           string
 	Amount       float64
@@ -21,4 +26,13 @@ func NewTransaction() *Transaction {
 	transaction.ID = uuid.NewV4().String()
 	transaction.CreatedAt = time.Now()
 	return transaction
+}
+
+func (transaction *Transaction) ProcessAndValidate(creditCard *CreditCard) {
+	if transaction.Amount+creditCard.Balance > creditCard.Limit {
+		transaction.Status = "rejected"
+	} else {
+		transaction.Status = "approved"
+		creditCard.Balance = creditCard.Balance + transaction.Amount
+	}
 }
