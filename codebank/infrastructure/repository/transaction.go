b27f7cd96358774284c42a2987ac1ca6edeadcc6
Marco Aurélio Messa <marco.messa@ltmfidelidade.com.br>
2021-07-06 07:35:54 -0300

    feat: adds transaction main methods
    - updateBalance
    - CreateCreditCard
    - GetCreditCard

--- codebank/infrastructure/repository/transaction.go
+++ codebank/infrastructure/repository/transaction.go
@@ -2,9 +2,10 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
+
 	"github.com/macmessa/imersao-fullcycle3/codebank/domain"
 )
-
 
 type TransactionRepositoryDb struct {
 	db *sql.DB
@@ -15,7 +16,7 @@ func NewTransactionRepositoryDb(db *sql.DB) *TransactionRepositoryDb {
 }
 
 func (t *TransactionRepositoryDb) SaveTransaction(transaction domain.Transaction, creditCard domain.CreditCard) error {
-	stmt, err := t.db.Prepare(query:`
+	stmt, err := t.db.Prepare(`
 		insert into transactions(id, credit_card_id, amount, status, description, store, created_at)
 		values($1, $2, $3, $4, $5, $6, $7)
 	`)
@@ -39,7 +40,11 @@ func (t *TransactionRepositoryDb) SaveTransaction(transaction domain.Transaction
 	}
 
 	if transaction.Status == "approved" {
-		// transaction logic here
+		err = t.updateBalance(creditCard)
+
+		if err != nil {
+			return err
+		}
 	}
 
 	err = stmt.Close()
@@ -49,4 +54,55 @@ func (t *TransactionRepositoryDb) SaveTransaction(transaction domain.Transaction
 	}
 
 	return nil
+}
+
+func (t *TransactionRepositoryDb) updateBalance(creditCard domain.CreditCard) error {
+	_, err := t.db.Exec(
+		"update credit_cards set balance = $1 where id = $2",
+		creditCard.Balance, creditCard.ID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *TransactionRepositoryDb) CreateCreditCard(creditCard domain.CreditCard) error {
+	stmt, err := t.db.Prepare(`insert into credit_cards(id, name, number, expiration_month,expiration_year, CVV,balance, balance_limit) 
+								values($1,$2,$3,$4,$5,$6,$7,$8)`)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(
+		creditCard.ID,
+		creditCard.Name,
+		creditCard.Number,
+		creditCard.ExpirationMonth,
+		creditCard.ExpirationYear,
+		creditCard.CVV,
+		creditCard.Balance,
+		creditCard.Limit,
+	)
+	if err != nil {
+		return err
+	}
+	err = stmt.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TransactionRepositoryDb) GetCreditCard(creditCard domain.CreditCard) (domain.CreditCard, error) {
+	var c domain.CreditCard
+	stmt, err := t.db.Prepare("select id, balance, balance_limit from credit_cards where number=$1")
+	if err != nil {
+		return c, err
+	}
+	if err = stmt.QueryRow(creditCard.Number).Scan(&c.ID, &c.Balance, &c.Limit); err != nil {
+		return c, errors.New("credit card does not exists")
+	}
+	return c, nil
 }
