e16416ee09ed32e44a807c12491ffd2a3335cf86
Marco Aurélio Messa <marco.messa@ltmfidelidade.com.br>
2021-07-07 08:11:11 -0300

    feat: adds kafka producer and sends transaction

--- codebank/usecase/process_transaction.go
+++ codebank/usecase/process_transaction.go
@@ -1,14 +1,17 @@
 package usecase
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/macmessa/imersao-fullcycle3/codebank/domain"
 	"github.com/macmessa/imersao-fullcycle3/codebank/dto"
+	"github.com/macmessa/imersao-fullcycle3/codebank/infrastructure/message_broker"
 )
 
 type UseCaseTransaction struct {
 	TransactionRepository domain.TransactionRepository
+	MessageProducer       message_broker.MessageProducer
 }
 
 func NewUseCaseTransaction(transactionRepository domain.TransactionRepository) UseCaseTransaction {
@@ -29,6 +32,20 @@ func (u UseCaseTransaction) ProcessTransaction(transactionDto dto.Transaction) (
 	transaction := u.newTransaction(transactionDto, ccBalanceAndLimit)
 	transaction.ProcessAndValidate(creditCard)
 	err = u.TransactionRepository.SaveTransaction(*transaction, *creditCard)
+
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+
+	transactionDto.ID = transaction.ID
+	transactionDto.CreatedAt = transaction.CreatedAt
+	transactionJson, err := json.Marshal(transactionDto)
+
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+
+	err = u.MessageProducer.Publish(string(transactionJson), "payments")
 
 	if err != nil {
 		return domain.Transaction{}, err
