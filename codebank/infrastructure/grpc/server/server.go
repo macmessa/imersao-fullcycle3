746e9951ac685698aa25e42c26583e647bfe9516
Marco Aurélio Messa <marco.messa@ltmfidelidade.com.br>
2021-07-08 10:03:35 -0300

    feat: add grpc server

---
+++ codebank/infrastructure/grpc/server/server.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"log"
+	"net"
+
+	"github.com/macmessa/imersao-fullcycle3/codebank/infrastructure/grpc/pb"
+	"github.com/macmessa/imersao-fullcycle3/codebank/infrastructure/grpc/service"
+	"github.com/macmessa/imersao-fullcycle3/codebank/usecase"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type GRPCServer struct {
+	ProcessTransactionUseCase usecase.UseCaseTransaction
+}
+
+func NewGRPCServer() GRPCServer {
+	return GRPCServer{}
+}
+
+func (g GRPCServer) Serve() {
+	listener, err := net.Listen("tcp", "0.0.0.0:50052")
+
+	if err != nil {
+		log.Fatalf("could not listen tcp port")
+	}
+	transactionService := service.NewTransactionService()
+	transactionService.ProcessTransactionUseCase = g.ProcessTransactionUseCase
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	pb.RegisterPaymentServiceServer(grpcServer, transactionService)
+	grpcServer.Serve(listener)
+}
